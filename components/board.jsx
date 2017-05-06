53bdf4fe8c411d4aed3d42421b4ac4581f134c88
Tu Ngo <tungo8810@gmail.com>
2017-05-05 17:02:49 -0700

    Add Board render method

--- components/board.jsx
+++ components/board.jsx
@@ -6,13 +6,15 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
 
-
+    console.log(this.props.board.grid);
   }
 
   render() {
     return (
       <div>
-        <Tile/>
+        {
+          this.props.board.grid.map((row, idx) => <div key={idx}>{row.map((cell, cIdx) => <div key={cIdx}></div>)}</div>)
+        }
       </div>
     );
   }
